9959bdaeef3ec10a26f47747e6b9281d3718ce57
Peter Tribble <peter.tribble@gmail.com>
2025-02-19 14:02:44 +0000

    Add package-info files

---
+++ org/tribblix/illuminate/explorer/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * SPDX-License-Identifier: CDDL-1.0
+ *
+ * CDDL HEADER START
+ *
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source. A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ *
+ * CDDL HEADER END
+ *
+ * Copyright 2025 Peter Tribble
+ *
+ */
+
+/**
+ * This package provides the explorer view of illuminate.
+ * <p>
+ * The implementation is based on an identifier for a given object,
+ * defined in the SysItem class.
+ */
+
+package org.tribblix.illuminate.explorer;
